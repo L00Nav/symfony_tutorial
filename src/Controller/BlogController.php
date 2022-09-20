a4f84933f98d3beeb1093179b193c4fe379e0931
TayakiNoTenshi <luixlt@yahoo.com>
2022-09-20 10:48:27 +0300

    routing - priority param

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog/{page}', name: 'blog_list', requirements: ['page' => '\d+'])]
+    public function list(int $page = 1): Response
+    {
+        // ...
+    }
+
+    #[Route('/blog/{slug}', name: 'blog_show')]
+    public function show(string $slug): Response
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        // ...
+    }
+}
